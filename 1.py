71e6ee1252310b0873ab9372a5412760a14d9d34
AtharvaGurao <140331187+AtharvaGurao@users.noreply.github.com>
2024-01-05 05:38:17 +0000

    1

--- 1.py
+++ 1.py
@@ -1,1 +1,13 @@
+def divide():
+    try:
+        x = int(input("First: "))
+        y = int(input("Second: "))
+        result = x / y
+        return result
+    except ValueError as e:
+        return f"Error: {e}. Please enter valid numbers."
+    except ZeroDivisionError as e:
+        return f"Error: {e}. Cannot divide by zero."
 
+for i in range(3):
+    print(divide())
