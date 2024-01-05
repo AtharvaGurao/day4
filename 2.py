3c6d9c5fbf2a30ae965021f82311c677fe6bf194
AtharvaGurao <140331187+AtharvaGurao@users.noreply.github.com>
2024-01-05 07:57:14 +0000

    23

--- 2.py
+++ 2.py
@@ -1,9 +1,9 @@
 #file handaling
-#f = open(file="sample.txt", mode="w")
-#f.write("ash chooses pikachu")
-#f.write("\n")
-#f.write("misty chooes psyduck")
-#f.close()
+f = open(file="sample.txt", mode="w")
+f.write("aash chooses pikachu")
+f.write("\n")
+f.write("misty chooes psyduck")
+f.close()
 
 #input number of lines from user. example if user chooes 5,write allthe pokemons to the file
 
@@ -12,7 +12,7 @@
 
 #for i in range(n):
  #   f.write(input("Pokemon:"))
-  #  f.write("/n")
+  #  f.write("\n")
 #f.close()
 
 
@@ -20,43 +20,43 @@
 # QUESTIONS.TXT that contains same questions 
 # ANSWER.TXT that contains the answer
 
-def make_questions(n,*questions):
-  f = open(file="QUESTIONS.TXT", mode="w")
-  for questions in questions: 
-    f.write("questions")
-    f.write("/n")
-  f.close()
+#def make_questions(n,*questions):
+ # f = open(file="QUESTIONS.TXT", mode="w")
+  #for questions in questions: 
+   # f.write("questions")
+    #f.write("/n")
+  #f.close()
 
-def write_answers(n,*answers):
-  f = open(file="ANSWER.TXT", mode="w")
-  for answer in answers: 
-    f.write("answer")
-    f.write("/n")
-  f.close()
+#def write_answers(n,*answers):
+ # f = open(file="ANSWER.TXT", mode="w")
+  #for answer in answers: 
+ #   f.write("answer")
+  #  f.write("/n")
+  #f.close()
 
-  make_questions("capital of india")
-  write_answers()
+  #make_questions("capital of india")
+  #write_answers()
 
   #main program
   #openthe files in resding mode
-try:
-  q=open("QUESTIONS.TXT","r")
-  a=open("ANSWER.TXT","r")
+#try:
+ # q=open("QUESTIONS.TXT","r")
+ # a=open("ANSWER.TXT","r")
 
-   correct,wrong =0,0
+  # correct,wrong =0,0
 
-  for questions,answer in zip(q,a):
-    user_answer =input(question)
-    if user_answer == answer:
-      correct+=1
-    else: wrong-=1
+  #for questions,answer in zip(q,a):
+   # user_answer =input(question)
+  #  if user_answer == answer:
+   #   correct+=1
+   # else: wrong-=1
 
-except FileNotFoundError as e:
-  print(e)
+#except FileNotFoundError as e:
+ # print(e)
 
 
-finally:
-  q.close()
-  a.close()
+#finally:
+#  q.close()
+#  a.close()
   
    
