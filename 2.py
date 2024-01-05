b9510c1143cb88a185a647a14bbb9acba590c197
AtharvaGurao <140331187+AtharvaGurao@users.noreply.github.com>
2024-01-05 05:44:34 +0000

    2

---
+++ 2.py
@@ -0,0 +1,8 @@
+#file handaling
+f = open(file="sample.txt", mode="w")
+f.write("ash chooses pikachu")
+f.write("\n")
+f.write("misty chooes psyduck")
+f.close()
+
+#input number of lines from user. example if user chooes 5,write allthe pokemons to the file
